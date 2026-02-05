75439d72046995585fcb658d511378d54aadeb70
Dineth Bandara <131736551+Dinethzz@users.noreply.github.com>
2026-02-05 23:39:06 +0530

    Fix authentication route registration and update CORS origin

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import connectDB from './config/mongodb.js';
+import authRouter from './routes/authRoutes.js';
 
 dotenv.config();
 
@@ -10,13 +11,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 connectDB();
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(cors({credentials: true, origin: 'http://localhost:4000'}));
 app.use(express.json());
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('Hellooo from the server!');
 });
+app.use('/api/auth', authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
