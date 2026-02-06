c449852ce7c3ec6415105ee499963243c7ac9d09
Dineth Bandara <131736551+Dinethzz@users.noreply.github.com>
2026-02-06 13:31:46 +0530

    Add routes for email verification and OTP sending in authRouter

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,12 +1,14 @@
 
 import express from 'express';
-import { logout, registerUser } from '../controllers/authController.js';
+import { logout, registerUser, sendVerifyOtp, verifyEmail} from '../controllers/authController.js';
 import { login } from '../controllers/authController.js';
-
+import userAuth from '../middleware/userAuth.js';
 
 const authRouter = express.Router();
 authRouter.post('/register', registerUser);
 authRouter.post('/login', login);
 authRouter.post('/logout', logout);
+authRouter.post('/send-verify-otp', userAuth, sendVerifyOtp);
+authRouter.post('/verify-account', userAuth, verifyEmail);
 
 export default authRouter;
